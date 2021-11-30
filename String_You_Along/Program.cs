620774b3ba8be4251816eb17da6c6f3771c05390
john cheney <john.cheney@student.centralia.edu>
2021-11-29 22:21:16 -0800

    Add project files.

---
+++ String_You_Along/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+// John Cheney
+// IT113
+// NOTES: Reverse: I was able to get the Reverse working by using a similar stack idea we played with during one of the mini assignments
+//
+//        Reverse Overload: This functions similar to the base, but in order to figure out placement of Uppercase letters I used a boolean array to track placement of the Uppercase
+//         letters and then set that place return the character as the Uppercase version
+//
+//        Symmetric: Functions using the Reverse method and then comparing if the strings are symmetrical
+//
+//        Equals: Just checks if, in the case of "Sunbeam Tiger", that they are the same string and if not a string it immediately returns false
+//
+//        ToString: I took the Hash idea we learned in class and set the string to a number. Then, with a little help from a classmate, I figured out how to
+//         make a dictionary to compare the values of the ASCII and return them to a variable as the words themselves
+//
+// BEHAVIORS NOT IMPLIMENTED AND WHY: All behaviors implemented
+namespace StringMaker_Cheney
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager manager = new StringManager();
+            
+            Console.WriteLine("Reverse w/out Preserving Case Location: " + manager.Reverse("Sunbeam Tiger"));
+            
+            Console.WriteLine("ToString() method returning ASCII: "+ manager);
+           
+            Console.WriteLine("Reverse Preserving Case Location: " + manager.Reverse("Sunbeam Tiger", true));
+           
+            Console.WriteLine("Is it Equal: " + manager.Equals("Sunbeam Tiger"));
+           
+            Console.WriteLine("Is it Symmetric: "+ manager.Symmetric("ABBA"));
+            
+            Console.WriteLine("Is it Symmetric: " + manager.Symmetric("ABA"));
+            
+            Console.WriteLine("Is it Symmetric: " + manager.Symmetric("ABba"));
+
+        }
+    }
+}
---
+++ String_You_Along/StringManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_Cheney
+{
+    class StringManager
+    {
+        private string _name;
+        public string Reverse(string s)
+        {
+            _name = s;
+            string temp = null;
+            Stack<char> stack = new(); 
+            for (int i = 0; i < s.Length; i++) 
+            {
+                stack.Push(s[i]); 
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                temp += stack.Pop(); 
+            } 
+            return temp;
+        }
+        public string Reverse(string s, bool PreserveCaseLocation)
+        {
+            if(!PreserveCaseLocation)
+            {
+                return Reverse(s);
+            }
+            else
+            {
+                _name = s;
+                string temp = null;
+                Stack<char> stack = new();
+                bool[] isCapital = new bool[s.Length];
+                for (int i = 0; i < s.Length; i++)
+                {  
+                    if(s[i].ToString() == s[i].ToString().ToUpper() && s[i] != ' ')
+                    {
+                        isCapital[i] = true;
+                    }
+                    else
+                    {
+                        isCapital[i] = false;
+                    }
+                    stack.Push(s[i]); 
+                }
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if(isCapital[i])
+                    {
+                        temp += stack.Pop().ToString().ToUpper();
+                    }
+                    else
+                    {
+                        temp += stack.Pop().ToString().ToLower();
+                    }
+                    
+                }
+                return temp;
+            }
+            
+        }
+        public bool Symmetric(string s)
+        {
+            _name = s;
+            if (s == Reverse(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            Dictionary<int, string> valuePairs = new();
+            valuePairs.Add(45,"Negative One");
+            valuePairs.Add(48, "Zero");
+            valuePairs.Add(49, "One");
+            valuePairs.Add(50, "Two");
+            valuePairs.Add(51, "Tree");
+            valuePairs.Add(52, "Four");
+            valuePairs.Add(53, "Five");
+            valuePairs.Add(54, "Six");
+            valuePairs.Add(55, "Seven");
+            valuePairs.Add(56, "Eight");
+            valuePairs.Add(57, "Nine");
+
+
+            string temp = null;
+            int ASCII = 0;
+            foreach (char c in _name)
+            {
+                ASCII += (int)c;
+            }
+            if (ASCII == 0)
+            {
+                return valuePairs[45];
+            }
+            
+            string sASCII = ASCII.ToString();
+            for(int i = 0; i < sASCII.Length; i++)
+            {
+                if(i == 0)
+                {
+                    temp = valuePairs[sASCII[i]];
+                }
+                else
+                {
+                    temp += " " + valuePairs[sASCII[i]];
+                }
+            }
+
+            return temp;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is string)
+            {
+                return _name == obj.ToString();
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+   
+}
